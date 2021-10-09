39e5b19908d2d4f933554464659949e7fa85a059
fusho-takahashi <zenryaku@fusho-takahashi.me>
2021-10-09 21:30:32 +0900

    2. Display a List

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Dynamight' },
+  { id: 12, name: 'Uravity' },
+  { id: 13, name: 'Ingenium' },
+  { id: 14, name: 'shoto' },
+  { id: 15, name: 'Red Riot' },
+  { id: 16, name: 'Froppy' },
+  { id: 17, name: 'Creati' },
+  { id: 18, name: 'Grape Juice' },
+  { id: 19, name: 'Chargebolt' },
+  { id: 20, name: 'Earphone Jack' },
+];
